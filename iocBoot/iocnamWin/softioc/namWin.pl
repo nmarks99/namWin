68d6ae40a9fc57a7afca1ac8cc36f1c22e0f647f
Nick Marks <45540813+nmarks99@users.noreply.github.com>
2025-08-15 11:25:16 -0500

    initial commit

---
+++ iocBoot/iocnamWin/softioc/namWin.pl
@@ -0,0 +1,119 @@
+#!/usr/bin/perl
+
+use strict;
+use FindBin;
+
+###################################################################
+# Set Environment Variables to be used by the rest of the scripts #
+###################################################################
+
+BEGIN
+{
+	# Top level of the IOC folder
+	$ENV{TOP} = "$FindBin::RealBin/../../..";
+	
+	
+	# IOC prefix name, used to find the correct iocBoot directory and executable
+	$ENV{IOC_NAME}="namWin";	
+	
+	
+	#########################
+	# IOC Executable Config #
+	#########################
+	
+	# Name of the IOC executable file
+	$ENV{IOC_BINARY}="$ENV{IOC_NAME}";
+	#! $ENV{IOC_BINARY}="namWin";
+	
+	
+	# Top-level bin directory for the IOC
+	$ENV{IOC_BIN_DIR}="$ENV{TOP}/bin";
+	#! $ENV{IOC_BIN_DIR}="/home/username/epics/synApps/support/namWin/bin";
+	#! $ENV{IOC_BIN_DIR}="/home/username/epics/synApps/support/module/iocs/exampleIOC/bin";
+	
+	# Architecture the IOC is built under
+	$ENV{EPICS_HOST_ARCH} //= "rhel9-x86_64";
+	#! $ENV{EPICS_HOST_ARCH}="rhel9-x86_64-debug";
+	
+	
+	# Full path to the IOC executable
+	$ENV{IOC_BIN_PATH}="$ENV{IOC_BIN_DIR}/$ENV{EPICS_HOST_ARCH}/$ENV{IOC_BINARY}";
+	
+	
+	
+	###########################
+	# IOC Startup File Config #
+	###########################
+	
+	# Startup Script for the IOC to run
+	$ENV{IOC_STARTUP_FILE}="st.cmd.Linux";
+	#! $ENV{IOC_STARTUP_FILE}="st.cmd.Cygwin";
+	#! $ENV{IOC_STARTUP_FILE}="st.cmd.Win32";
+	#! $ENV{IOC_STARTUP_FILE}="st.cmd.Win64";
+	
+	
+	# Directory that contains the startup script
+	$ENV{IOC_STARTUP_DIR}="$ENV{TOP}/iocBoot/ioc$ENV{IOC_NAME}";
+	#! $ENV{IOC_STARTUP_DIR}="/home/username/epics/ioc/synApps/namWin/iocBoot/iocnamWin";
+	
+	
+	# Full path to the startup file
+	$ENV{IOC_STARTUP_FILE_PATH}="$ENV{IOC_STARTUP_DIR}/$ENV{IOC_STARTUP_FILE}";
+	
+	
+	
+	##########################
+	# Config For This Script #
+	##########################
+	
+	# Disable base's IOC shell history 
+	$ENV{EPICS_IOCSH_HISTFILE}="";
+	
+	# Directory that contains all the command modules to be loaded
+	$ENV{IOC_COMMAND_DIR}="$ENV{IOC_STARTUP_DIR}/softioc/commands";
+	#! $ENV{IOC_COMMAND_DIR}="/home/username/epics/ioc/synApps/namWin/iocBoot/iocnamWin/softioc/commands";
+	
+	
+	$ENV{IOC_CMD}="$ENV{IOC_BIN_PATH} $ENV{IOC_STARTUP_FILE_PATH}";
+	
+	# Required shell commands
+	$ENV{SCREEN}="screen";
+	$ENV{TELNET}="telnet";
+	$ENV{PROCSERV}="/APSshare/bin/procServ";
+	$ENV{NETCAT}="nc";
+	$ENV{ECHO}="echo";
+	
+	# Remove EPICS 7.0.8 history
+	$ENV{EPICS_IOCSH_HISTFILE}="";
+}
+
+
+use Env;
+use File::Spec;
+use Module::Loaded;
+use List::Util;
+
+use lib "$IOC_COMMAND_DIR";
+use _info;
+use _commands;
+
+
+#####################################################################
+
+my $GET_SCREEN_PID=1;
+
+#####################################################################
+
+if (! -d $ENV{IOC_COMMAND_DIR})
+{
+	print("Error: IOC command directory ($ENV{IOC_COMMAND_DIR}) doesn't exist.\n");
+	print("IOC_COMMAND_DIR in $FindBin::RealScript needs to be corrected.\n");
+	die;
+}
+
+#####################
+#    Parse Input    #
+#####################
+
+_commands::call("_local", @ARGV);
+
